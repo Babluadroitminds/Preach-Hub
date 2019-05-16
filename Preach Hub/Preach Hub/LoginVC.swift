986ef456a373718898e9da5b8b0ce4bc67396244
thripthi <thripthi@adroitminds.com>
2019-05-16 17:53:52 +0530

    Login and registration design

---
+++ Preach Hub/Preach Hub/LoginVC.swift
@@ -0,0 +1,17 @@
+//
+//  LoginVC.swift
+//  Preach Hub
+//
+//  Created by Sajeev Sasidharan on 16/05/19.
+//  Copyright © 2019 AdroitMinds. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
---
+++ Preach Hub/Preach Hub/RegistrationVC.swift
@@ -0,0 +1,17 @@
+//
+//  RegistrationVC.swift
+//  Preach Hub
+//
+//  Created by Sajeev Sasidharan on 16/05/19.
+//  Copyright © 2019 AdroitMinds. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationVC: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
