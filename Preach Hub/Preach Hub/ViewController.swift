0c47e32f7f70704b37672e2b80df4b3c01addbfe
Bablu s nair <bablu.snair@adroitminds.com>
2019-05-16 15:09:49 +0530

    initial checkin

---
+++ Preach Hub/Preach Hub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Preach Hub
+//
+//  Created by APPLE on 16/05/19.
+//  Copyright © 2019 AdroitMinds. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
